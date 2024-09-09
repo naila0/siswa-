0652100cce64aab977656888a499ae2a505b2c87
naila0 <nailaf677@gmail.com>
2024-09-09 10:08:30 +0700

    update

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const basisdata = getFirestore(app);
 
 
 export async function ambilDaftarSiswa() {
